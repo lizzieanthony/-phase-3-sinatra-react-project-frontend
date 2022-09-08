c0faafbbdf41128985c635a4e1a9cc248d139523
Lizzie <lizziewrab@me.com>
2022-09-08 13:37:17 -0500

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Button, makeStyles } from '@material-ui/core';
 
-function App() {
+// const useStyles = makeStyles({
+//   button:{
+//   backgroundColor: 'red'
+//   }
+// })
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>Hello World</h1>
+     {/* <button variant="contained" className={classes.button}>Click Me!</button> */}
     </div>
   );
 }
