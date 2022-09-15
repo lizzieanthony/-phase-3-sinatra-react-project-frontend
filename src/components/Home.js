284f4d252cb7777b0c8e053bc9d0c259e447cb83
Lizzie <lizziewrab@me.com>
2022-09-15 14:54:57 -0500

    initial fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,22 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import WorkoutList from "./WorkoutList";
 
 const Home = () => {
-   const [workouts, setWorkouts] = useState([
-    {name: "Monday", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.", id: 1},
-    {name: "Tuesday", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.", id: 2}
-   ])
-    // const [workouts, setWorkouts] = useState([]);
+   const [workouts, setWorkouts] = useState(null)
 
-    // useEffect(() => {
-    //     fetch("http://localhost:9292/workouts")
-    //       .then((r) => r.json())
-    //       .then((workouts) => setWorkouts(workouts));
-    //   }, []);
+   useEffect(() => {
+    fetch("http://localhost:9292/workouts")
+      .then((r) => r.json())
+      .then((workouts) => setWorkouts(workouts));
+  }, []);
 
 
     return ( 
         <div className="home">
-            <WorkoutList workouts={workouts}/>
+            {workouts && <WorkoutList 
+            workouts={workouts} 
+            header="All Workouts" 
+            />}
         </div>
      );
 }
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,9 +1,9 @@
 
-const  WorkoutList = (props) => {
-    const workouts = props.workouts;
+const  WorkoutList = ({workouts, header}) => {
     
     return ( 
         <div className="workout-list">
+            <h2>{header}</h2>
             {workouts.map((workout) => (
                 <div className="workout-preview" key={workout.id}> 
                     <h2>{workout.name}'s Workout</h2>
